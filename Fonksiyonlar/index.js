16ba9c2f561946fc7eaa95605ea0b7ffef3a5e45
Hakan Yargıcı <hakanyargici@Hakan.local>
2024-03-14 02:23:02 +0300

    Javascript'in DOM yapılarının bulunduğu klasör eklendi.

---
+++ Fonksiyonlar/index.js
@@ -0,0 +1,9 @@
+// 1- Kendisine gönderilen kelimeyi, istenilen kez ekrana yazdırın.
+
+// 2- Dikdörtgenin alanı ve çevresini hesaplayan formul'ü yazın.
+
+// 3- Yazı tura uygulamasını fonksiyon kullanarak yazınınz.
+
+// 4- Kendisine gönderilen sayının tam bölenlerini dizi şeklinde döndüren fonksiyonu yazınınz.
+
+// 5- Değişken sayıda parametre alan toplam isminde bir fonksiyon tanımlayınız.
