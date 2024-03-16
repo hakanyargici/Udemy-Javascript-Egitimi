4bcf34f882c22c29fdb196bd88117578369a060f
Hakan Yargıcı <hakanyargici@Hakan.local>
2024-03-16 15:10:30 +0300

    Script.js dosyasına checkPassword ve checkLength özellikleri eklendi.

--- Form Validator Uygulaması/script.js
+++ Form Validator Uygulaması/script.js
@@ -29,6 +29,7 @@ function validateEmail(input){
     }
 }
 
+// form içerik kontrolü
 function checkRequired(validate) {
     validate.forEach(function(input) {
         if (input.value === ''){
@@ -40,10 +41,35 @@ function checkRequired(validate) {
     });
 }
 
-// input içerik kontrolü ve ux bildirim
+function checkLength(input, min, max){
+    if(input.value.length < min){
+        error(input, `${input.id} en az ${min} karakter kadar olmalıdır.`)
+    } else if (input.value.length > max) {
+        error(input, `${input.id} en az ${max} karakter kadar olmalıdır.`)
+    } else {
+        success(input);
+    }
+}
+
+function checkPassword(input1, input2){
+    if(input1.value !== input2.value){
+        error(input2, `Parolalar Eşleşmiyor!`);
+    } 
+    if (input2.value !== input1.value){
+        error(input1, `Parolalar Eşleşmiyor!`);
+    }
+}
+
+// Form çıktısını form ile ilişkilendirme
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
     checkRequired([userName, email, password, repassword]);
     validateEmail(email)
+
+    checkLength(userName, 3, 15);
+    checkLength(password, 7, 12);
+    checkLength(repassword, 7, 12);
+
+    checkPassword(password, repassword);
 });
